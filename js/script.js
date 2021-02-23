127c4f60a46c83d5f03a6ed50b9a69a6a47d9953
belmoz <64534509+belmoz@users.noreply.github.com>
2021-02-23 16:56:49 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,71 @@
 // Подключение файлов =====================================================
+// SlideToggle
+let _slideUp = (target, duration = 500) => {
+    target.style.transitionProperty = 'height, margin, padding';
+    target.style.transitionDuration = duration + 'ms';
+    target.style.height = target.offsetHeight + 'px';
+    target.offsetHeight;
+    target.style.overflow = 'hidden';
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    window.setTimeout(() => {
+        target.style.display = 'none';
+        target.style.removeProperty('height');
+        target.style.removeProperty('padding-top');
+        target.style.removeProperty('padding-bottom');
+        target.style.removeProperty('margin-top');
+        target.style.removeProperty('margin-bottom');
+        target.style.removeProperty('overflow');
+        target.style.removeProperty('transition-duration');
+        target.classList.remove('_slide');
+    }, duration);
+}
+let _slideDown = (target, duration = 500) => {
+    target.style.removeProperty('display');
+    let display = window.getComputedStyle(target).display;
+    if (display === 'none')
+        display = 'flex';
+
+    target.style.display = display;
+    let height = target.offsetHeight;
+    target.style.overflow = 'hidden';
+    target.style.height = 0;
+    target.style.paddingTop = 0;
+    target.style.paddingBottom = 0;
+    target.style.marginTop = 0;
+    target.style.marginBottom = 0;
+    target.offsetHeight;
+    target.style.transitionProperty = 'height, margin, padding';
+    target.style.transitionDuration = duration + 'ms';
+    target.style.height = height + 'px';
+    target.style.removeProperty('padding-top');
+    target.style.removeProperty('padding-bottom');
+    target.style.removeProperty('margin-top');
+    target.style.removeProperty('margin-bottom');
+    window.setTimeout(() => {
+        target.style.removeProperty('height');
+        target.style.removeProperty('overflow');
+        target.style.removeProperty('transition-duration');
+        target.style.removeProperty('transition-property');
+        target.classList.remove('_slide');
+    }, duration);
+}
+let _slideToggle = (target, duration = 500) => {
+    if (!target.classList.contains('_slide')) {
+        target.classList.add('_slide');
+        if (window.getComputedStyle(target).display === 'none') {
+            return _slideDown(target, duration);
+        } else {
+            return _slideUp(target, duration);
+        }
+    }
+}
+// ==============================================
+// inMobile
+let _inMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent);
 // слайдеры  ===================================================
 let dayChecked = document.querySelectorAll(".selectDay__input");
 let daySlider = document.querySelector(".selectDay__slider");
@@ -107,17 +174,21 @@ for (let i = 0; i < completedCounts.length; i++) {
 		lostHour.innerHTML = (new Date((norm * day - completedCount.value) / norm * 3600000)).getHours() - 2 + ":" + ('0' + (new Date((norm * day - completedCount.value) / norm * 3600000)).getMinutes()).slice(-2);
 
 		if (completedCount.value > norm * day) {
-			lostHour.innerHTML = "максимум " + Math.floor(norm * day);
+			lostHour.innerHTML = "максимум " + (norm * day);
 		}
 
 
 
 	});
 }
-// определение нормы в день относительно рабочих часов и типа ленты
 
+let infoSpoller = document.querySelector('.info__spoller');
+let infoBlock = document.querySelector('.info__block');
 
-//определние остатка часов до нормы
-// let lostHours1 = (new Date((dayNorm(typeStrip) - completedCount1) / normHour * 3600000)).getHours() - 2 + ":" + (new Date((dayNorm(typeStrip1) - completedCount1) / normHour * 3600000)).getMinutes();
+infoSpoller.addEventListener('click', function (e) {
+	this.classList.toggle('_active');
+	// infoBlock.classList.toggle('_active');
+	_slideToggle(infoBlock);
+});
 
 //=================================================================================================================
